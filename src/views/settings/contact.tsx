4936c50d82f1494c2dfb769047766a34d504fd89
ease <easeava@gmail.com>
2022-04-16 22:08:34 +0800

    feat: 店铺联系我们ui完成

--- src/views/settings/contact.tsx
+++ src/views/settings/contact.tsx
@@ -1,4 +1,5 @@
 import { SpaceView } from '@/components/space-view';
+import { NButton, NCard, NCascader, NForm, NFormItem, NInput, NInputNumber, NRadio, NRadioGroup, NSpace } from 'naive-ui';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -11,7 +12,35 @@ export default defineComponent({
   render () {
     return (
       <SpaceView>
-        联系我们
+        <NCard size='small'>
+          <NForm labelPlacement='left' labelWidth={120}>
+            <NFormItem label='客服电话：'>
+              <NSpace vertical class='mt-2'>
+                <NRadioGroup defaultValue='tel'>
+                  <NRadio name='type' value='tel'>座机号</NRadio>
+                  <NRadio name='type' value='mobile'>手机号</NRadio>
+                </NRadioGroup>
+
+                <NSpace align='center'>
+                  <NInputNumber style={{width: '60px'}} placeholder={'区号'} showButton={false} />
+                  -
+                  <NInputNumber placeholder={'座机号'} showButton={false} />
+                </NSpace>
+              </NSpace>
+            </NFormItem>
+
+            <NFormItem label='联系地址：'>
+              <NCascader style={{width: '300px'}} />
+            </NFormItem>
+
+            <NFormItem label='详细地址：'>
+              <NSpace>
+                <NInput placeholder='请填写详细地址，以便买家联系；（勿重复填写省市区信息）' style={{width: '480px'}}/>
+                <NButton>搜索地图</NButton>
+              </NSpace>
+            </NFormItem>
+          </NForm>
+        </NCard>
       </SpaceView>
     );
   }
