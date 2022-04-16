13dd237c077f60589896b3e869e284fcad6849b2
ease <easeava@gmail.com>
2022-04-16 22:24:17 +0800

    chore: add member action

---
+++ src/views/member/action/index.tsx
@@ -0,0 +1,18 @@
+import { SpaceView } from '@/components/space-view';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MemberAction',
+
+  setup () {
+
+  },
+
+  render () {
+    return (
+      <SpaceView>
+        客户Action
+      </SpaceView>
+    );
+  }
+});
