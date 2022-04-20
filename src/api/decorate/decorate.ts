86a0200655911e74f1f9438381fa53049d356363
ease <easeava@gmail.com>
2022-04-20 22:22:35 +0800

    feat: 增加装修页面Api, 与后端交互

---
+++ src/api/decorate/decorate.ts
@@ -0,0 +1,24 @@
+import { http } from '@/utils/http';
+
+export const create = (data: any): Promise<any> => {
+  return http.request<any>({
+    url: '/decorate',
+    method: 'POST',
+    data
+  });
+};
+
+export const update = (id: number | string, data: any): Promise<void> => {
+  return http.request<void>({
+    url: `/decorate/${id}`,
+    method: 'PUT',
+    data
+  });
+};
+
+export const getLists = (): Promise<any[]> => {
+  return http.request<any[]>({
+    url: '/decorate',
+    method: 'GET'
+  });
+};
--- src/views/diy/decorate.tsx
+++ src/views/diy/decorate.tsx
@@ -1,5 +1,5 @@
 import { defineComponent, onMounted, ref } from 'vue';
-import { MenuOption, NButton, NLayout, NLayoutHeader, NLayoutSider, NMenu, NSpace } from 'naive-ui';
+import { MenuOption, NButton, NLayout, NLayoutHeader, NLayoutSider, NMenu, NSpace, useMessage } from 'naive-ui';
 import { FreeLayout, Free, FreeTitleTextWidget, FreeWhiteHeightWidget } from 'free-core';
 import { AsideGroup, PageDataSchemas } from 'free-core/lib/types/core/src/interface';
 import NutuiImageAdWidget from '@/components/free-nutui/image-ad';
@@ -9,8 +9,10 @@ import NutuiNoticeBarhWidget from '@/components/free-nutui/notice-bar';
 import NutuiVideoPlayerWidget from '@/components/free-nutui/video-player';
 // import NutuiNavigationWidget from '@/components/free-nutui/navigation';
 // import NutuiGoodsCardWidget from '@/components/free-nutui/goods-card';
+import { create as createDecorate } from '@/api/decorate/decorate';
 
 import './style.scss';
+import { useRouter } from 'vue-router';
 
 const asideGroups: AsideGroup[] = [
   {
@@ -42,7 +44,11 @@ export default defineComponent({
   name: 'Diy',
 
   setup () {
-    const freeLayRef = ref();
+    const message = useMessage();
+    const loading = ref(false);
+    const router = useRouter();
+
+    const freeLayoutRef = ref();
 
     const menus: MenuOption[] = [
       {
@@ -70,23 +76,78 @@ export default defineComponent({
           }
         ],
       };
-      freeLayRef.value.setPageData(pageData);
+      freeLayoutRef.value.setPageData(pageData);
     });
+
+    // 获取页面数据
+    function getPageData () {
+      const datas = freeLayoutRef.value.getPageData() as any;
+      const page = datas.page.map((item: any) => {
+        return {
+          key: item.widgetKey,
+          data: item.model
+        };
+      });
+      const pageData: any = {
+        page
+      };
+      Object.values(datas.core).forEach((item: any) => {
+        pageData[item.widgetKey] = item.model || {};
+      });
+
+      console.log(JSON.stringify(pageData));
+      const freePage = pageData['free-page'];
+
+      return {
+        data: pageData,
+        title: freePage.title,
+        remark: freePage.description,
+      };
+    }
+
+    async function handleSubmit (e: MouseEvent, status = false) {
+      console.log(status);
+      loading.value = true;
+      try {
+        await createDecorate(
+          {
+            ...getPageData(),
+            status
+          }
+        );
+        message.success('提交成功');
+        router.replace({
+          path: '/store/page'
+        });
+      } catch (error) {
+        console.log(error);
+      }
+      loading.value = false;
+    }
 
     return {
       menus,
-      freeLayRef
+      loading,
+      freeLayoutRef,
+      handleSubmit
     };
   },
 
   render () {
+    const {
+      loading,
+      handleSubmit
+    } = this;
+
     return (
       <NLayout position="absolute">
         <NLayoutHeader style="height: 56px; padding: 0 24px;" bordered>
-          <NSpace justify='center' align='center' style={{ height: '100%' }}>
-            <NSpace size={[50, 0]}>
-              <NButton type='primary' secondary style={{ padding: '0 40px' }}>预览</NButton>
-              <NButton type='primary' style={{ padding: '0 40px' }}>保存</NButton>
+          <NSpace justify='space-between' align='center' style={{ height: '100%' }}>
+            <div></div>
+            <NSpace size={[20, 0]}>
+              <NButton size='small' type='primary' secondary style={{ padding: '0 20px' }}>预览</NButton>
+              <NButton size='small' type='primary' style={{ padding: '0 20px' }} loading={loading} onClick={e => handleSubmit(e, false)}>保存</NButton>
+              <NButton size='small' type='primary' style={{ padding: '0 20px' }}>发布</NButton>
             </NSpace>
           </NSpace>
         </NLayoutHeader>
@@ -104,7 +165,7 @@ export default defineComponent({
             </NMenu>
           </NLayoutSider>
           <NLayout>
-            <FreeLayout asideGroup ref="freeLayRef" />
+            <FreeLayout asideGroup ref="freeLayoutRef" />
           </NLayout>
         </NLayout>
       </NLayout>
